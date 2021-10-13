90e9bd618f0faecdfd23ade3325fd64b5da888ec
AlmadelX <almadel@almadelx.ru>
2021-10-13 18:27:06 +0500

    Done onboarding

--- Cleaning/Cleaning/Views/OnboardingPageView.swift
+++ Cleaning/Cleaning/Views/OnboardingPageView.swift
@@ -16,13 +16,53 @@ struct OnboardingPageView: View {
                 .ignoresSafeArea()
             
             VStack {
-                Image("Onboarding" + String(page))
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
+                HStack {
+                    Spacer()
+                    
+                    Image("Onboarding" + String(page))
+                        .resizable()
+                        .scaledToFit()
+                }
                 
-                Spacer()
+                Text(model.title)
+                    .font(.system(size: 36))
+                    .fontWeight(.bold)
+                    .foregroundColor(model.titleColor)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 35)
+                    .padding(.bottom, 5)
+                Text(model.text)
+                    .font(.system(size: 17))
+                    .fontWeight(.semibold)
+                    .foregroundColor(model.textColor)
+                    .opacity(model.textOpacity)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 35)
+                    .padding(.bottom, 35)
+                
+                ZStack {
+                    Group {
+                        if page == pages.count - 1 {
+                            RoundedRectangle(cornerRadius: 15)
+                                .frame(width: 60, height: 60)
+                                .foregroundColor(model.buttonColor)
+                                .onTapGesture {
+                                    appState.showOnboarding = false
+                                }
+                        } else {
+                            NavigationLink(destination: OnboardingPageView(pages: pages, page: page + 1)) {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .frame(width: 60, height: 60)
+                                    .foregroundColor(model.buttonColor)
+                            }
+                        }
+                    }
+                    
+                    Image("OnboardingButtonArrow" + String(page))
+                }
+                .padding(.bottom, 30)
             }
-            .ignoresSafeArea()
+            .padding(.top)
             
             VStack {
                 HStack {
@@ -30,46 +70,25 @@ struct OnboardingPageView: View {
                         .frame(width: 9, height: 9)
                         .foregroundColor(model.indicatorColor)
                         .opacity(model.indicatorsOpacities[0])
-                        .padding(.horizontal, 6)
+                        .padding(.horizontal, 5)
                     Circle()
                         .frame(width: 9, height: 9)
                         .foregroundColor(model.indicatorColor)
                         .opacity(model.indicatorsOpacities[1])
-                        .padding(.horizontal, 6)
+                        .padding(.horizontal, 5)
                     Circle()
                         .frame(width: 9, height: 9)
                         .foregroundColor(model.indicatorColor)
                         .opacity(model.indicatorsOpacities[2])
-                        .padding(.horizontal, 6)
+                        .padding(.horizontal, 5)
                         
                     Spacer()
                 }
                 .padding()
                 
                 Spacer()
-                
-                Text(model.title)
-                    .font(.system(size: 32))
-                    .fontWeight(.bold)
-                    .foregroundColor(model.titleColor)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 35)
-                
-                if page == pages.count - 1 {
-                    RoundedRectangle(cornerRadius: 15)
-                        .frame(width: 60, height: 60)
-                        .foregroundColor(model.buttonColor)
-                        .onTapGesture {
-                            appState.showOnboarding = false
-                        }
-                } else {
-                    NavigationLink(destination: OnboardingPageView(pages: pages, page: page + 1)) {
-                        RoundedRectangle(cornerRadius: 15)
-                            .frame(width: 60, height: 60)
-                            .foregroundColor(model.buttonColor)
-                    }
-                }
             }
+            .padding(.top)
         }
         .navigationBarHidden(true)
         .navigationBarBackButtonHidden(true)
@@ -87,6 +106,9 @@ struct OnboardingPageView_Previews: PreviewProvider {
             indicatorsOpacities: [1.0, 0.4, 0.4],
             title: "Блестящее качество",
             titleColor: Color(R: 255, G: 255, B: 255),
+            text: "Наши клинеры - настоящие мастера своего дела, а используемые нами материалы сертифицированны и безопасны",
+            textColor: Color(R: 220, G: 224, B: 242),
+            textOpacity: 1.0,
             buttonColor: Color(R: 255, G: 255, B: 255)
         )
     ]
--- Cleaning/Cleaning/Views/OnboardingView.swift
+++ Cleaning/Cleaning/Views/OnboardingView.swift
@@ -11,6 +11,9 @@ struct OnboardingView: View {
             indicatorsOpacities: [1.0, 0.4, 0.4],
             title: "Блестящее качество",
             titleColor: Color(R: 255, G: 255, B: 255),
+            text: "Наши клинеры - настоящие мастера своего дела, а используемые нами материалы сертифицированны и безопасны",
+            textColor: Color(R: 220, G: 224, B: 242),
+            textOpacity: 1.0,
             buttonColor: Color(R: 255, G: 255, B: 255)
         ),
         OnboardingPageModel(
@@ -22,6 +25,9 @@ struct OnboardingView: View {
             indicatorsOpacities: [0.5, 1.0, 0.2],
             title: "Эко-френдли клининг",
             titleColor: Color(R: 65, G: 65, B: 69),
+            text: "После каждой уборки мы забираем ваш мусор, сортируем его и увозим на специализированные свалки.",
+            textColor: Color(R: 77, G: 86, B: 136),
+            textOpacity: 0.7,
             buttonColor: Color(R: 54, G: 86, B: 249)
         ),
         OnboardingPageModel(
@@ -33,6 +39,9 @@ struct OnboardingView: View {
             indicatorsOpacities: [0.5, 0.5, 1.0],
             title: "Небывалая доступность",
             titleColor: Color(R: 53, G: 52, B: 51),
+            text: "Клининг никогда не был таким доступным. Вы можете заказывать уборку несколько раз в месяц",
+            textColor: Color(R: 143, G: 140, B: 138),
+            textOpacity: 1.0,
             buttonColor: Color(R: 254, G: 180, B: 113)
         )
     ]
