6d0199a2afa5c7e0cbdb286c064327369f7a834a
AlmadelX <almadel@almadelx.ru>
2021-10-13 07:49:04 +0500

    Initial commit

---
+++ Cleaning/Cleaning/CleaningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleaningApp.swift
+//  Cleaning
+//
+//  Created by Матвей Алексеев on 13.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct CleaningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
